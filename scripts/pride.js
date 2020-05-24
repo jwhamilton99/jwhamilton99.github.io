489f7d93eca832a1a413643bc297389063a65460
Justin Hamilton <jwhamilton99@gmail.com>
2020-05-23 23:47:11 -0400

    refined pride scripts

--- scripts/pride.js
+++ scripts/pride.js
@@ -6,15 +6,16 @@ if(d.getMonth() == 5) {
 		var color1 = colors[Math.floor(Math.random()*(colors.length))];
 		var gradString = "";
 		if(color1 == "rainbow") {
-			gradString = "linear-gradient(to bottom left, "+colors[0]+","+colors[1]+","+colors[2]+","+colors[3]+","+colors[4]+","+colors[5]+")";
+			gradString = "linear-gradient(to bottom left, ";
+			for(var i = 0; i < colors.length-1; i++) {
+				gradString+=(colors[i]+",");
+			}
+			gradString = (gradString.substring(0,gradString.length-1))+")";
 			document.body.style.setProperty("--background-end-position-x","100%");
 		} else {
 			var color2 = color1;
 			while(color2 == color1 || color2 == "rainbow") {
 				color2 = colors[Math.floor(Math.random()*(colors.length))];
-			}
-			if(color2 == "rainbow" || color2 == color1) {
-				c++;
 			}
 			gradString = "linear-gradient(to bottom left, "+color1+","+color2+","+color1+","+color2+","+color1+")";
 		}
