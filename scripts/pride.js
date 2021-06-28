08d84d60d5538d084329d662df9a9a271d205a90
Justin Hamilton <jwhamilton99@gmail.com>
2021-06-28 03:56:54 +0000

    custom email, css vars, pride colors are back

--- scripts/pride.js
+++ scripts/pride.js
@@ -1,9 +1,6 @@
 //Happy Pride Month!
 
 function setFlag() {
-	var white = "#bfbfbf";
-		var black = "#595959";
-	
 		var blue = "#1a75ff";
 		var lightBlue = "#3399ff";
 		var purple = "#ff00ff";
@@ -22,26 +19,30 @@ function setFlag() {
 		var trad = [red, orange, yellow, green, lightBlue, purple];
 		var bi = [pink,purple,blue];
 		var pan = [lightPink,yellow,lightBlue];
-		var ace = [black,white,purple];
+		var ace = [purple];
 		var intersex = [yellow, purple];
-		var trans = [lightBlue,lightPink+" 40%",white+" 50%",lightPink+" 60%",lightBlue];
-		var genderfluid = [pink, white, purple, black, blue];
-		var genderqueer = [purple, white, darkGreen];
+		var trans = [lightBlue,lightPink,lightBlue];
+		var genderfluid = [pink, purple, blue];
+		var genderqueer = [purple, darkGreen];
 		var poly = [pink, darkGreen, blue];
-		var agender = [black, white, green, white, black];
-		var aro = [darkGreen, green, white, black];
-		var enby = [yellow, white, purple, black];
-		var lesbian = [darkPurple, lightPurple, pink, white, lightPink, lightRed, darkRed];
+		var agender = [green];
+		var aro = [darkGreen, green];
+		var enby = [yellow, purple];
+		var lesbian = [darkPurple, lightPurple, pink, lightPink, lightRed, darkRed];
 		
 		var flagStrings = ["Gay", "Bisexual", "Pansexual","Asexual","Intersex","Transgender","Genderfluid","Genderqueer","Polysexual","Agender","Aromantic","Nonbinary","Lesbian"];
 		var flags = [trad, bi, pan, ace, intersex, trans, genderfluid, genderqueer, poly, agender, aro, enby, lesbian];
 		var links = ["https://lgbt.wikia.org/wiki/Gay_and_Lesbian_Pride_Month","https://lgbt.wikia.org/wiki/Bisexuality","https://lgbt.wikia.org/wiki/Pansexuality","https://lgbt.wikia.org/wiki/Asexuality","https://lgbt.wikia.org/wiki/Intersexuality","https://gender.wikia.org/wiki/Transgender","https://gender.wikia.org/wiki/Gender_Fluid","https://gender.wikia.org/wiki/Genderqueer","https://lgbt.wikia.org/wiki/Polysexuality","https://gender.wikia.org/wiki/Agender","https://lgbt.wikia.org/wiki/Aromantic","https://gender.wikia.org/wiki/Non-binary","https://lgbt.wikia.org/wiki/Lesbian"];
 		
-		// document.body.style.setProperty("--background-end-position-x","100%");
-		
 		var flagIndex = Math.floor(Math.random()*flags.length)
 		
 		var flag = flags[flagIndex];
+		
+		var color = flag[0];
+		
+		if(flag.length < 1) {
+			color = flag[Math.floor(Math.random()*flag.length)]
+		}
 		
 		if(document.getElementById("indexContent") != null) {
 			if(flag == trad) {
@@ -50,16 +51,8 @@ function setFlag() {
 				document.getElementById("indexContent").innerHTML+=" Happy <a href='"+links[flagIndex]+"'>"+flagStrings[flagIndex]+"</a> Pride!";
 			}
 		}
-		
-		var gradString = "linear-gradient(to bottom left, ";
-		
-		for(c in flag) {
-			gradString+=(flag[c]+",");
-		}
-		
-		gradString = (gradString.substring(0,gradString.length-1))+")";
 	
-		// document.body.style.setProperty("--link-gradient", gradString);
+		document.body.style.setProperty("--link-color", color);
 		
 		return flagStrings[flagIndex];
 }
